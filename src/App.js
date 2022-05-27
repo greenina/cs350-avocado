cf850e75def12259729bedc9e4b729aec7e5925c
greenina <igreen0485@kaist.ac.kr>
2022-05-28 03:56:19 +0900

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from './Pages/Home';
+import Login from './Pages/Login'
+import Search from './Pages/Search'
+import Timer from './Pages/Timer'
+import Riding from './Pages/Riding'
+import Billing from './Pages/Billing'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element = {<Home/>}/>
+        <Route path="/login" element = {<Login/>} />
+        <Route path="/search" element = {<Search/>} />
+        <Route path="/timer" element = {<Timer/>} />
+        <Route path="/riding" element = {<Riding/>} />
+        <Route path="/billing" element = {<Billing/>} />
       </Routes>
     </BrowserRouter>
   );
