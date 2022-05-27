6d44d1472663013ff7d088f545537c65149fc619
greenina <igreen0485@kaist.ac.kr>
2022-05-28 03:58:59 +0900

    Set page for riding

---
+++ src/Pages/Riding.js
@@ -0,0 +1,12 @@
+import React, { useEffect } from 'react'
+import './Riding.css'
+import {auth} from '../firebase'
+
+const Riding = () =>{
+    return(
+    <div>
+        RIDING PAGE
+    </div>)
+}
+
+export default Riding; 
