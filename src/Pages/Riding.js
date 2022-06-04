1c543ef06601396de8d0e4edb3850d7fe54fd173
jjjunyeong <jjjunyeong@gmail.com>
2022-06-05 00:26:30 +0900

    add pause riding btn

--- src/Pages/Riding.js
+++ src/Pages/Riding.js
@@ -6,15 +6,21 @@ import { useNavigate } from "react-router-dom";
 import riding from '../Assets/riding.gif'
 
 const Riding = () =>{let navigate = useNavigate(); 
-    const routeChange = () =>{ 
+    const routeChangeToBill = () =>{ 
+      let path = '/billing'; 
+      navigate(path);
+    }
+    const routeChangeToMap = () =>{ 
+      //TODO: 맵 중간 페이지로 이어주기
       let path = '/billing'; 
       navigate(path);
     }
     return(
     <div className="riding" align="center" >
         <img src={riding} alt='loading...'/>
-        <p>You cannot see the map while riding a kickboard</p>
-        <Button variant="outlined" onClick={routeChange}>End Riding</Button>
+        <p>**You cannot see the map while riding a kickboard**</p>
+        <p><Button variant="outlined" onClick={routeChangeToMap}>Pause Riding</Button></p>
+        <p><Button variant="outlined" onClick={routeChangeToBill}>End Riding</Button></p>
     </div>)
 }
 
